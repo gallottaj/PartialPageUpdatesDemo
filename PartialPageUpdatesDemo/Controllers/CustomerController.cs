dd064f50b08f7b202bd839c50a06fa242324295d
Joey <gallo@DESKTOP-BO1GFI1>
2021-09-28 17:54:30 -0700

    displaying initial list of customers

---
+++ PartialPageUpdatesDemo/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialPageUpdatesDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialPageUpdatesDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+        public CustomerController()
+        {
+            customers.Add(new CustomerModel { Id = 0, Name = "Sherry", Age = 42 });
+            customers.Add(new CustomerModel { Id = 2, Name = "Jerry", Age = 22 });
+            customers.Add(new CustomerModel { Id = 3, Name = "Kerry", Age = 56 });
+            customers.Add(new CustomerModel { Id = 4, Name = "Mherry", Age = 49 });
+            customers.Add(new CustomerModel { Id = 5, Name = "Therry", Age = 19 });
+        }
+    
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+    }
+}
